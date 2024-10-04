8b5daaa73142bc346bc467326845b8c9d6c88c9d
Alex Grover <hello@alexgrover.me>
2024-10-04 10:58:03 -0400

    Fix protocol for local dev (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next"
 import { getFrameMetadata } from "frog/next"
 
 export async function generateMetadata(): Promise<Metadata> {
-  const url = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL || "localhost:3000"}`
+  const url = process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : "http://localhost:3000"
   const frameMetadata = await getFrameMetadata(`${url}/api`)
   return {
     other: frameMetadata,
