d578d36f45b2f088466bbbfaaf66e621aec2bd2e
zachterrell57 <zachterrell@gmail.com>
2024-10-07 20:26:49 -0500

    fix colocation

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import { getFrameMetadata } from "frog/next";
 import type { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const url = process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : "http://localhost:3000";
+  const url = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL || "http://localhost:3000"}`;
   const frameMetadata = await getFrameMetadata(`${url}/api`);
   return {
     other: frameMetadata,
